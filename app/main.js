091730e6e6f9fd22139fd405dc68c6b9bcacbab1
Jairo <jairoportela@vah.com.br>
2016-02-02 18:28:59 -0200

    Stuffs

--- app/main.js
+++ app/main.js
@@ -3,9 +3,8 @@ import {render} from 'react-dom';
 import Greeter from "./Greeter";
 
 import './main.css';
+import './material.min.css';
 
 import 'material-design-lite/material.min.js'
-
-import 'material-design-lite/material.min.css';
 
 render(<Greeter/>, document.getElementById('root'));
